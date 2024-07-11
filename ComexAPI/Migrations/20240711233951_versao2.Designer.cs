7905bf6e35e081aabcb865fe874808785978af43
Fabio T. Onishi <ftonishi@hotmail.com>
2024-07-11 20:40:37 -0300

    Atividades da semana 12

--- ComexAPI/Migrations/20240711233951_versao2.Designer.cs
+++ ComexAPI/Migrations/20240711233951_versao2.Designer.cs
@@ -3,6 +3,7 @@ using ComexAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ComexAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240711233951_versao2")]
+    partial class versao2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
